0aa47529c260d5b0ad2720ffa34f19b79e11ae8f
dejagraver <dejaamaartin@gmail.com>
2021-05-28 23:08:27 -0400

    debugging

---
+++ routes/HTMLroutes.js
@@ -0,0 +1,3 @@
+// GET /notes should return the notes.html file.
+
+// GET * should return the index.html file.
---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+
+//creating express' server
+const app = express();
+
+//instantiate server and tell it to listen for a request 
+const PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(express.static("public"));
+
+// api and HTML routes 
+// const APIroutes = require('./routes/APIroutes');
+// const HTMLroutes = require('./routes/HTMLroutes');
+// app.use('/', APIroutes);
+// app.use('/', HTMLroutes);
+
+//listen for the PORT
+app.listen(PORT, () => {
+    console.log(`Server listening on PORT: ${PORT}`);
+});
